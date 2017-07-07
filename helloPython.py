fc6990817c4ca1db3347ff9138ba0e3206423382
ImportNewXXT0101 <793336588@qq.com>
2017-07-07 23:58:38 +0800

    Initial commit

---
+++ helloPython.py
@@ -0,0 +1,22 @@
+
+
+def deom_string():
+    stra = 'hello there!'
+    print 1,stra.capitalize()
+    print 2,stra.lower()
+    print 3,stra.replace("l",'e')
+
+    strb = '\r\nwe lcome\n have\rfun!'
+    print 4,strb.lstrip()
+    print 5,strb.rsplit()
+    print 6,'_:_'.join(['q','w','e'])
+    print 7,strb.split(" ")
+
+def demo_list():
+    lista=[1,2,4,'s']
+    print 1,lista
+    print 2,1 in lista
+
+if __name__=='__main__':
+    # deom_string()
+    demo_list()
